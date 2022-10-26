3cc3a2655682c292c70cf203a99bdb22c93970e8
Oleksandr Pikush <pikush.oleksandr@gmail.com>
2022-10-26 23:47:10 +0300

    1+2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const newGallery = document.querySelector('.gallery');
+const galleryItemsMarkup = galleryItemsMarkupCreate(galleryItems);
+
+newGallery.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+
+function galleryItemsMarkupCreate(pics) {
+    return pics
+    .map((pic) => {
+      const { preview, original, description } = pic;
+      return `
+    <div class="gallery__item">
+     <a class="gallery__link" href="${original}">
+        <img 
+        class="gallery__image"
+        src=${preview}
+        data-source=${original}
+        alt=${description}/>
+     </a>
+        </div>`;
+    })
+    .join("");
+}
+
+newGallery.addEventListener("click", selectPic);
+
+function selectPic(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains("gallery__image")) {
+        return;
+    }
+
+    const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}", width = "800", height = "600">`
+  );
+    instance.show();
+    
+
+    newGallery.addEventListener("keydown", (evt) => {
+      if (evt.key === "Escape") {
+        instance.close();
+      }
+    });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const newGallery = document.querySelector('.gallery');
+const galleryItemsMarkup = galleryItemsMarkupCreate(galleryItems);
+
+newGallery.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+
+function galleryItemsMarkupCreate(pics) {
+    return pics
+        .map((pic) => 
+    `<a class="gallery__item" href="${pic.original}">
+    <img class="gallery__image"
+    src="${pic.preview}" 
+    alt="${pic.description}" />
+    </a>`
+    )
+    .join("");
+}
+
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log(galleryItems);
